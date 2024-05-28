b7c36929dd22ce5ccd6ecf1bbcfeb97a33468a4e
Ramiro Berrelleza <ramiro@okteto.com>
2024-05-28 14:52:09 -0700

    upgrade to version 1.20.1

--- upgrade.sh
+++ upgrade.sh
@@ -2,7 +2,7 @@ doppler secrets download --format=env --no-file > okteto.env
 source okteto.env
 
 helm repo update
-helm upgrade ramiro okteto/okteto --version "1.19.1" -f values.yaml -n=okteto --devel \
+helm upgrade ramiro okteto/okteto --version "1.20.1" -f values.yaml -n=okteto --devel \
     --set auth.openid.clientId=$OPENID_CLIENTID \
     --set auth.openid.endpoints.issuer=$OKTA_ISSUER \
     --set auth.openid.endpoints.authorization=$OKTA_AUTHORIZATION \
@@ -12,8 +12,9 @@ helm upgrade ramiro okteto/okteto --version "1.19.1" -f values.yaml -n=okteto --
     --set github.clientId=$GITHUB_CLIENTID \
     --set github.clientSecret=$GITHUB_CLIENTSECRET \
     --set github.installationUrl=$GITHUB_INSTALLATION_URL \
-    --set cluster.endpoint=https://104.199.117.34 \
     --set insights.enabled=true \
-    --set insights.bearerSecret.key=ramiro 
+    --set insights.bearerSecret.key=ramiro
+    #--set ingress-nginx.enabled=false --set okteto-nginx.enabled=false --set defaultBackend.enabled=false --set ingress.oktetoIngressClass=traefik --set ingress.class=traefik --set ingress.ip=10.3.252.190
+
 
 rm okteto.env
