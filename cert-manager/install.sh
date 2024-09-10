836b6fae35fd715e8e5a52955322b2975f7c7082
Ramiro <ramiro@okteto.com>
2024-09-10 16:00:03 -0700

    move cluster to ramiro.okteto.me

--- cert-manager/install.sh
+++ cert-manager/install.sh
@@ -1,6 +1,7 @@
 helm upgrade --install \
   cert-manager jetstack/cert-manager \
-  --namespace okteto \
-  --version v1.11.2 \
-  --set installCRDs=true \
-  --set global.podSecurityPolicy.enabled=true
+  --namespace cert-manager \
+  --create-namespace \
+  --version v1.15.3 \
+  --set crds.enabled=true \
+  --values values.yaml
