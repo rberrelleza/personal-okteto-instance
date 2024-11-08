55fbaf273849a1e63b5efe00ff2a447a478825c8
Ramiro <ramiro@okteto.com>
2024-11-08 11:10:05 -0800

    Upgrade to 1.26

--- upgrade.sh
+++ upgrade.sh
@@ -1,2 +1,2 @@
 helm repo update
-helm upgrade okteto okteto/okteto --version "1.25.0" -f config.yaml -n=okteto --devel
+helm upgrade okteto okteto/okteto --version "1.26.0" -f config.yaml -n=okteto --devel
